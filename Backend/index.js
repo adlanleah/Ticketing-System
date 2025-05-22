ce612515b15b3de666f8f7783c58d7d23e6e797a
Adlan Leah Kirumira <adlanleah01@gmail.com>
2025-05-22 17:35:15 +0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -22,6 +22,7 @@ app.use(cors({
 }));
 
 
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -42,4 +43,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
